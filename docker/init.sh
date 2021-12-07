075dc0d6ce2c00c152d11835b0619312b14acbe5
turpin_l <turpin_l@etna-alternance.net>
2021-12-07 09:41:02 +0100

    [UPDATE]update mysql docker file for init db and fix permissions

--- docker/init.sh
+++ docker/init.sh
@@ -2,3 +2,9 @@ docker-compose up -d --build
 docker-compose run php composer create-project symfony/website-skeleton .
 sudo chown -R $USER ../symfony
 docker-compose up -d
+docker-compose run php ./bin/console cache:clear --no-warmup --env=prod
+docker-compose run php ./bin/console cache:clear --no-warmup --env=dev
+docker-compose run php ./bin/console cache:warmup --env=prod
+docker-compose run php ./bin/console cache:warmup --env=dev
+sudo chmod -R 777 ../symfony/var/cache
+sudo chmod -R 777 ../symfony/var/log
