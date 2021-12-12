203979ce4f7f2b176f0b07de192ac73dc3131fbe
turpin_l <turpin_l@etna-alternance.net>
2021-12-12 21:46:02 +0100

    [FEATURE]adding comendians/cients gen

--- symfony/src/Controller/ProfileController.php
+++ symfony/src/Controller/ProfileController.php
@@ -2,22 +2,22 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
 use App\Form\UserType;
 use App\Repository\UserRepository;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use App\Security\Voter\ProfileVoter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Security;
 
 class ProfileController extends AbstractController
 {
+
+
     #[Route('/profile/{id}', name: 'profile_show')]
     public function index(int $id, UserRepository $userRepository): Response
     {
         $profile = $userRepository->find($id);
-        $this->denyAccessUnlessGranted("profile_view", $profile);
+        $this->denyAccessUnlessGranted(ProfileVoter::VIEW, $profile);
         $form = $this->createForm(UserType::class);
         return $this->render('profile/profile.html.twig', [
             'form' => $form->createView(),
--- symfony/src/Repository/UserRepository.php
+++ symfony/src/Repository/UserRepository.php
@@ -36,6 +36,16 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
         $this->_em->flush();
     }
 
+    public function filterByRole(string $role)
+    {
+        $role = mb_strtoupper($role);
+        return $this->createQueryBuilder("u")
+            ->andWhere('JSON_CONTAINS(u.roles, :role) = 1')
+            ->setParameter('role', '"ROLE_' . $role . '"')
+            ->getQuery()
+            ->getResult();
+
+    }
     // /**
     //  * @return User[] Returns an array of User objects
     //  */
