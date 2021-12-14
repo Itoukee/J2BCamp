34675d470bb68742d94c12f2d9988f33f5354b46
turpin_l <turpin_l@etna-alternance.net>
2021-12-14 01:34:55 +0100

    [update]handle address

--- symfony/assets/js/searchbox.js
+++ symfony/assets/js/searchbox.js
@@ -9,6 +9,17 @@ function initSearchInput(id) {
 
 function onPlaceChanged() {
     let place = this.getPlace();
+    for(let i in place.address_components) {
+        let comp = place.address_components[i]
+        for (let j in comp.types) {
+            // Some types are ["country", "political"]
+            let type_element = document.getElementById(comp.types[j]);
+
+            if (type_element) {
+                type_element.value = comp.long_name;
+            }
+        }
+    }
 }
 
 google.maps.event.addDomListener(window, 'load', function() {
