b4ad9b4b83a6b5b6714e8b2e705bc87fc3971aee
pauche_m <pauche_m@etna-alternance.net>
2021-12-14 16:27:06 +0100

    add update for profile

--- symfony/src/Controller/ProfileController.php
+++ symfony/src/Controller/ProfileController.php
@@ -4,10 +4,14 @@ namespace App\Controller;
 
 use App\Form\UpdateProfileType;
 use App\Form\UserType;
+use App\Repository\CompaniesRepository;
 use App\Repository\UserRepository;
 use App\Security\Voter\ProfileVoter;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\AddressType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfileController extends AbstractController
@@ -25,6 +29,24 @@ class ProfileController extends AbstractController
             'form' => $form->createView(),
             'updateProfile' => $updateProfile->createView(),
             'profile' => $profile
+        ]);
+    }
+    #[Route('/profile/{id}', name: 'app_company_edit')]
+    public function edit(int $id, Request $request, UserRepository $userRepository, EntityManagerInterface $entityManager): Response
+    {
+        $user = $userRepository->find($id);
+        $form = $this->createForm(UpdateProfileType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('profile', ["id" => $user->getId()]);
+        }
+
+        return $this->render('company/edit.html.twig', [
+            'registrationForm' => $form->createView(),
         ]);
     }
 }
