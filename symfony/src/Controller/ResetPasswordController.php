ca251d9decdeecffccb26457bee07d447b994bc8
garo_n <garo_n@etna-alternance.net>
2021-12-14 23:31:56 +0100

    [SUCCESS] RESET PASSWORD WITH MAIL SECURE

--- symfony/src/Controller/ResetPasswordController.php
+++ symfony/src/Controller/ResetPasswordController.php
@@ -134,37 +134,37 @@ class ResetPasswordController extends AbstractController
         $user = $this->entityManager->getRepository(User::class)->findOneBy([
             'email' => $emailFormData,
         ]);
-
         // Do not reveal whether a user account was found or not.
         if (!$user) {
             return $this->redirectToRoute('app_check_email');
+
         }
 
         try {
             $resetToken = $this->resetPasswordHelper->generateResetToken($user);
+
         } catch (ResetPasswordExceptionInterface $e) {
+
             // If you want to tell the user why a reset email was not sent, uncomment
             // the lines below and change the redirect to 'app_forgot_password_request'.
             // Caution: This may reveal if a user is registered or not.
             //
-            // $this->addFlash('reset_password_error', sprintf(
-            //     'There was a problem handling your password reset request - %s',
-            //     $e->getReason()
-            // ));
+            $this->addFlash('reset_password_error', sprintf(
+            'There was a problem handling your password reset request - %s',
+            $e->getReason()
+            ));
 
             return $this->redirectToRoute('app_check_email');
         }
 
         $email = (new TemplatedEmail())
-            ->from(new Address('agentJ2B@gmail.com', 'AgentJ2B'))
+            ->from(new Address('agentj2b@gmail.com', 'AgentJ2B'))
             ->to($user->getEmail())
             ->subject('Your password reset request')
             ->htmlTemplate('reset_password/email.html.twig')
             ->context([
                 'resetToken' => $resetToken,
-            ])
-        ;
-
+            ]);
         $mailer->send($email);
 
         // Store the token object in session for retrieval in check-email route.
