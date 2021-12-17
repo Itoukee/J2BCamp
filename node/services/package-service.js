4c0856aaf6b5d6d0a718ffa3ac0b679976530b1f
turpin_l <turpin_l@etna-alternance.net>
2021-12-17 11:24:53 +0100

    adding somes comments

--- node/services/package-service.js
+++ node/services/package-service.js
@@ -1,10 +1,11 @@
+//on peut definir les intervalles de kilometrage avec un prix associé
 const kilometersPackage = new Map()
 kilometersPackage.set("<101", 55)
 kilometersPackage.set(">100|<506", 165)
 kilometersPackage.set(">505|<901", 225)
 kilometersPackage.set(">899", 275)
 
-
+//permet de verifier si une distance est dans un range specifié
 function checkValidity(km, range) {
     let intervals = range.split("|")
     return intervals.every((curr) => {
@@ -12,6 +13,7 @@ function checkValidity(km, range) {
     })
 }
 
+//a partir d'une distance, on recupere le forfait kilometrage
 function getPriceFromKm(km) {
     let final = 0
     kilometersPackage.forEach((value, key) => {
